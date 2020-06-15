aeaa269723c3aeae367f51106bb0da72c046edf2
saherla <saherla03@gmail.com>
2020-06-15 01:49:20 -0500

    Initial commit

---
+++ sign_in_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in_app/src/pages/home_page.dart';
+import 'package:sign_in_app/src/pages/login_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'APP',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
