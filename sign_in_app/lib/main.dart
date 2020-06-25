9d0def62e422bd071df38014f338d1b0d137ae02
saherla <saherla03@gmail.com>
2020-06-25 01:59:19 -0500

    Add login bloc and streams, WIP

--- sign_in_app/lib/main.dart
+++ sign_in_app/lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_app/src/blocs/provider.dart';
 import 'package:sign_in_app/src/pages/home_page.dart';
 import 'package:sign_in_app/src/pages/login_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'APP',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-        'home': (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'APP',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+        ),
+      ),
     );
   }
 }
--- sign_in_app/lib/src/pages/login_page.dart
+++ sign_in_app/lib/src/pages/login_page.dart
@@ -1,31 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_app/src/blocs/provider.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
-        children: <Widget>[
-          _backgroundCreate(size),
-          _loginForm(size)
-        ],
+        children: <Widget>[_backgroundCreate(context), _loginForm(context)],
       ),
     );
   }
 
-  Widget _backgroundCreate(Size size) {
+  Widget _backgroundCreate(BuildContext context) {
+    final size = MediaQuery.of(context).size;
 
     final backgroundPurple = Container(
       height: size.height * 0.4,
       width: double.infinity,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: <Color> [
-            Color.fromRGBO(63, 63, 156, 1.0),
-            Color.fromRGBO(90, 70, 178, 1.0),
-          ]
-        ),
+        gradient: LinearGradient(colors: <Color>[
+          Color.fromRGBO(63, 63, 156, 1.0),
+          Color.fromRGBO(90, 70, 178, 1.0),
+        ]),
       ),
     );
 
@@ -46,107 +42,128 @@ class LoginPage extends StatelessWidget {
         Positioned(child: circle, bottom: -50.0, right: -10.0),
         Positioned(child: circle, bottom: 120.0, right: 20.0),
         Positioned(child: circle, bottom: -20.0, left: -20.0),
-
         Container(
-        padding: EdgeInsets.only(top: 80.0),
-        child: Column(
-          children: <Widget>[
-            Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0,),
-            SizedBox(height:10.0, width: double.infinity),
-            Text('Salvador Hernández', style: TextStyle(color: Colors.white, fontSize: 25.0))
-          ],
-        ),
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(
+                Icons.person_pin_circle,
+                color: Colors.white,
+                size: 100.0,
+              ),
+              SizedBox(height: 10.0, width: double.infinity),
+              Text('Salvador Hernández',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0))
+            ],
+          ),
         )
       ],
     );
   }
 
-  Widget _loginForm(Size size) {
+  Widget _loginForm(BuildContext context) {
+    final bloc = Provider.of(context);
+    final size = MediaQuery.of(context).size;
+
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
           SafeArea(
-            child: Container(
-              height: 180.0
-            ),
+            child: Container(height: 180.0),
           ),
           Container(
             width: size.width * 0.85,
             margin: EdgeInsets.symmetric(vertical: 30.0),
             padding: EdgeInsets.symmetric(vertical: 50.0),
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3.0,
-                  offset: Offset(0.0, 5.0),
-                  spreadRadius: 3.0
-                )
-              ]
-            ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 5.0),
+                      spreadRadius: 3.0)
+                ]),
             child: Column(
               children: <Widget>[
                 Text('Ingreso', style: TextStyle(fontSize: 20.0)),
-                SizedBox(height: 60.0,),
-                _createEmail(),
-                SizedBox(height: 30.0,),
-                _createPassword(),
-                SizedBox(height: 30.0,),
+                SizedBox(
+                  height: 60.0,
+                ),
+                _createEmail(bloc),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _createPassword(bloc),
+                SizedBox(
+                  height: 30.0,
+                ),
                 _createButton(),
-                SizedBox(height: 30.0,),
+                SizedBox(
+                  height: 30.0,
+                ),
               ],
             ),
           ),
-
           Text('Forgot Password'),
-          SizedBox(height: 100.0,)
+          SizedBox(height: 100.0),
         ],
       ),
     );
   }
 
-  Widget _createEmail() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
-          hintText: 'example@email.com',
-          labelText: 'Email',
-        ),
-      ),
+  Widget _createEmail(LoginBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.emailStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+                hintText: 'example@email.com',
+                labelText: 'Email',
+                counterText: snapshot.data),
+            onChanged: bloc.changeEmail,
+          ),
+        );
+      },
     );
   }
 
-    Widget _createPassword() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
-          labelText: 'Password',
-        ),
-      ),
+  Widget _createPassword(LoginBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.passwordStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+              labelText: 'Password',
+              counterText: snapshot.data,
+            ),
+            onChanged: bloc.changePassword,
+          ),
+        );
+      },
     );
   }
 
   Widget _createButton() {
     return RaisedButton(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-        child: Text('Access')
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      elevation: 0.0,
-      color: Colors.deepPurple,
-      textColor: Colors.white,
-      onPressed: () {} 
-    );
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Access')),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        elevation: 0.0,
+        color: Colors.deepPurple,
+        textColor: Colors.white,
+        onPressed: () {});
   }
 }
